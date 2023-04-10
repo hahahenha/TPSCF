86e3adc2ab6c27083a3aa82080203f06ce2d000f
Randolph Han <hahahenha@126.com>
2023-04-10 22:55:01 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 
 # Depends on: static_signalling.py, plot_metrics.py
 
-from algorithm.distributed_PPO import Distr_PPO
+from algorithm.Alg import Distr_PPO
 from sumo_related import sumo_env
 from utils import plot_metrics
 
-# Train the QR-DQN model
+# Train the model
 sumo_env.endis_sumo_guimode(1)
-Nruns = 100
+Nruns = 25
 Distr_PPO(Nruns)
 print("finish 1")
 
