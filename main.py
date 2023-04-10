7e7e854a1f5fb16cd5b4a850190878fda1266794
Randolph Han <hahahenha@126.com>
2023-04-10 22:57:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 
 # Depends on: static_signalling.py, plot_metrics.py
 
-from algorithm.Alg import Distr_PPO
+from algorithm.Alg import algorithm
 from sumo_related import sumo_env
 from utils import plot_metrics
 
 # Train the model
 sumo_env.endis_sumo_guimode(1)
 Nruns = 25
-Distr_PPO(Nruns)
+algorithm(Nruns)
 print("finish 1")
 
 
